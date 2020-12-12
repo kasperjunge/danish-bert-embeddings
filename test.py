668fb30625b7f3dbfe1faec790dbe79da4792e7c
KasperJuunge <kasperjuunge@gmail.com>
2020-12-12 10:25:39 +0100

    added test script

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 from danish_bert_embeddings import DanishBertEmbeddings
 
-from danish_bert_embeddings.danish_bert_embeddings import DanishBertEmbeddings
+from danish_bert_embeddings import DanishBertEmbeddings
 
 embedder = DanishBertEmbeddings()
-embedding = embedder.embed('hvad er penis')
-print(embedding)
+embedding = embedder.embed('En mega fed embedding.')
